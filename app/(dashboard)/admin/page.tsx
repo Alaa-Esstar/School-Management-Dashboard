7fb2fe565ce73e9903044a2c24bc845b278874d5
Alaa Esstar <alaa.esstar@gmail.com>
2024-11-09 20:56:03 +0100

    Admin Screen

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -1,7 +1,39 @@
+import Announcements from "@/app/components/Announcements";
+import AttendenceChart from "@/app/components/AttendanceChart";
+import CountChart from "@/app/components/CountChart";
+import EventCalendar from "@/app/components/EventCalendar";
+import FinanceChart from "@/app/components/FinanceChart";
+import UserCard from "@/app/components/UserCard";
+
 const AdminPage = () => {
     return (
-        <div>
-            admin
+        <div className="p-4 flex flex-col gap-4 md:flex-row">
+            <div className="w-full lg:w-2/3 flex flex-col gap-8">
+                <div className="flex gap-4 justify-between flex-wrap">
+                    <UserCard type="student" />
+                    <UserCard type="teacher" />
+                    <UserCard type="parent" />
+                    <UserCard type="staff" />
+                </div>
+                <div className="flex gap-4 flex-col lg:flex-row">
+                    {/* Count Chart */}
+                    <div className="w-full lg:w-1/3 h-[450px]">
+                        <CountChart />
+                    </div>
+                    {/* Atendence Chart */}
+                    <div className="w-full lg:w-2/3 h-[450px]">
+                        <AttendenceChart />
+                    </div>
+                </div>
+                {/* boom chart */}
+                <div className="w-full h-[500px]">
+                    <FinanceChart />
+                </div>
+            </div>
+            <div className="w-full lg:w-1/3 flex flex-col gap-8">
+                <EventCalendar />
+                <Announcements />
+            </div>
         </div>
     );
 }
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -9,15 +9,15 @@ export default function DashboardLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <div className="h-screen flex">
-            <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]">
+        <div className="min-h-screen flex">
+            <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
                 <Link href="/" className="flex items-center justify-center gap-2 lg:justify-start">
                     <Image src="/logo.png" alt="logo" width={32} height={32} />
-                    <span className="hidden lg:block">SchooLama</span>
+                    <span className="hidden lg:block font-bold">SchooLama</span>
                 </Link>
                 <Menu />
             </div>
-            <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+            <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA]">
                 <Navbar />
                 {children}
             </div>
